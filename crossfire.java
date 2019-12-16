6fbc6a69c2b2a3c92246cee32d4c75f04f0d6911
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-16 16:48:53 +0200

    Add files via upload

---
+++ crossfire.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class crossfire {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int[] dim = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int rows = dim[0];
+        int cols = dim[1];
+        List<List<Integer>> matrix = generateMatrix(rows, cols);
+
+        String command = scanner.nextLine();
+        while(!command.equals("Nuke it from orbit")){
+            int[] target = Arrays.stream(command.split(" ")).mapToInt(Integer::parseInt).toArray();
+            int r = target[0];
+            int c = target[1];
+            int radius = target[2];
+            for (int i = r-radius; i <= r+radius; i++) {
+                if(isInBounds(matrix, c, i)){
+                    matrix.get(i).set(c, 0);
+                }
+            }
+            for (int i =  c-radius; i <= c+radius ; i++) {
+                if(isInBounds(matrix,i, r)){
+                    matrix.get(r).set(i, 0);
+                }
+            }
+            for (int i = 0; i < matrix.size(); i++) {
+                int j = 0;
+                while (j<matrix.get(i).size()){
+                    if(matrix.get(i).get(j)==0){
+                        matrix.get(i).remove(j);
+                    }else{
+                        j++;
+                    }
+                }
+                if (matrix.get(i).size()==0){
+                    matrix.remove(r);
+                    r--;
+                }
+            }
+            command = scanner.nextLine();
+        }
+
+
+        printMatrix(matrix);
+    }
+
+    private static boolean isInBounds(List<List<Integer>> matrix, int cols, int rows) {
+        if (rows>=0 && rows<matrix.size()){
+            if(cols < matrix.get(rows).size() && cols>=0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void printMatrix(List<List<Integer>> matrix) {
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix.get(i).size(); j++) {
+                System.out.print(matrix.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static List<List<Integer>> generateMatrix(int rows, int cols) {
+        int num = 1;
+        List<List<Integer>> matrix = new ArrayList<>();
+        for (int i = 0; i < rows; i++) {
+            List<Integer> curr = new ArrayList<>();
+            matrix.add(curr);
+            for (int j = 0; j < cols; j++) {
+                matrix.get(i).add(j, num);
+                num++;
+            }
+        }
+        return matrix;
+    }
+}
---
+++ parking.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class parking {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int[] dem = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        boolean[][]lot = new boolean[dem[0]][dem[1]];
+        for (int i = 0; i < lot.length; i++) {
+            for (int j = 0; j < lot[i].length; j++) {
+                lot[i][j] = true;
+            }
+        }
+        String iinput;
+        while (!"stop".equals(iinput=scanner.nextLine())){
+            boolean isParked = false;
+            int[]tokkens = Arrays.stream(iinput.split(" ")).mapToInt(Integer::parseInt).toArray();
+            int steps = 1;
+            int rowEntrence = tokkens[0];
+            int row = tokkens[1];
+            int col = tokkens[2];
+
+            if(isFull(row, lot)){
+                System.out.printf("Row %d full%n", row);
+                continue;
+            }
+            steps+=col;
+            if(row!=rowEntrence){
+                steps+=Math.abs(row-rowEntrence);
+            }
+            if(lot[row][col]){
+                lot[row][col]=false;
+                System.out.println(steps);
+            }else{
+                for (int i = 1; i <lot[row].length; i++) {
+                    if(valid(row, col-i, lot) && lot[row][col-i]){
+                        steps -= i;
+                        lot[row][col-i]=false;
+                        System.out.println(steps);
+                        break;
+                    }else if (valid(row, col+i, lot) && lot[row][col+i]){
+                        steps+=i;
+                        lot[row][col+i]=false;
+                        System.out.println(steps);
+                        break;
+                    }
+                    }
+                }
+            }
+        }
+
+    private static boolean isFull(int row, boolean[][] lot) {
+        for (int i = 1; i < lot[row].length; i++) {
+            if(lot[row][i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean valid(int row, boolean[][] lot) {
+        return row>=0 && row<lot.length;
+    }
+
+    private static boolean valid(int row, int col, boolean[][] lot) {
+        return row>=0 && row<lot.length && col>0 && col<lot[row].length;
+    }
+
+}
+
