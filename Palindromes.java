152da122c9507ac9760108731ccb4309701d8517
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:20:06 +0200

    Matrices

--- Palindromes.java
+++ Palindromes.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
-public class palindromes {
+
+public class Palindromes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
         int[] info = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
