a036d5c56d234a6f215dadbfcf95e1513ac8e89d
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:08:55 +0200

    Update and rename bunnies.java to Bunnies.java

--- Bunnies.java
+++ Bunnies.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class bunnies {
+public class Bunnies {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
         int[] dem = Arrays.stream(scanner.nextLine().split(" "))
