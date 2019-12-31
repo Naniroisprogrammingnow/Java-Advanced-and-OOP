b6fa7e94b135976157cef805a4a0f880d9f57254
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:12:16 +0200

    Matrices

--- Crossfire.java
+++ Crossfire.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class crossfire {
+public class Crossfire {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
         int[] dim = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
