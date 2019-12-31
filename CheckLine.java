0c8e1b88de0f72f5b829356fec4ce36c440dd270
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:11:45 +0200

    StacksAndQueues

--- CheckLine.java
+++ CheckLine.java
@@ -1,7 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Scanner;
-public class checkLine {
+public class CheckLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
