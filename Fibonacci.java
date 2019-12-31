aa68ab338643d44ff4bc21bd47dba5a58988c2aa
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:14:57 +0200

    StacksAndQueues

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,7 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class fibonacci {
+public class Fibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
         int n = Integer.parseInt(scanner.nextLine());
