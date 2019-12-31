20158d9fab3f59ff9d47d2fa6743e26b4c91941a
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:18:47 +0200

    StacksAndQueues

--- MathPotato.java
+++ MathPotato.java
@@ -2,7 +2,7 @@ import java.util.ArrayDeque;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class mathPotato {
+public class MathPotato {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
