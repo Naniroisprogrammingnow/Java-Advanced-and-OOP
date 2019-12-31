c0cafcfa056ab15b7335c653a9fdd3772da74682
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:27:20 +0200

    StacksAndQueues

--- TextEdit.java
+++ TextEdit.java
@@ -1,7 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class textEdit {
+public class TextEdit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
         ArrayDeque<String> done = new ArrayDeque<>();
