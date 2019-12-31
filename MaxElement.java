110efffada26697caaca3af1549e6d9bd5dfd7e3
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:19:32 +0200

    StacksAndQueues

--- MaxElement.java
+++ MaxElement.java
@@ -1,7 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class maxEl {
+public class MaxElement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
         ArrayDeque<String> stack = new ArrayDeque<>();
