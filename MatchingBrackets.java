fea54ab187e20a5b7a3f90be7bcca919cd98d159
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:18:23 +0200

    StacksAndQueues

--- MatchingBrackets.java
+++ MatchingBrackets.java
@@ -1,7 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class matchingBrackets {
+public class MatchingBrackets {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
