a50687afe9be71ae5580fba8b36e9f75d9dd404a
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:21:57 +0200

    StacksAndQueues

--- PrinterQueue.java
+++ PrinterQueue.java
@@ -1,7 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class printerQueue {
+public class PrinterQueue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
