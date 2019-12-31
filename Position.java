14fa3a2902bfdf45c0ac7714aec1e8dcb750c684
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:21:27 +0200

    Matrices

--- Position.java
+++ Position.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
-public class position {
+public class Position {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
