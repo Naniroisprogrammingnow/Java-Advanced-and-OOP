256fd9c2b9fd527465663ad62a4112c081220a19
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:10:45 +0200

    StacksAndQueues

--- BrowserHistory.java
+++ BrowserHistory.java
@@ -1,7 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class browserHistory {
+public class BrowserHistory {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
