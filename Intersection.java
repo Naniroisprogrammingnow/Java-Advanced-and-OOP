c1b71619a74e10fb483ed2e866a5ccdc1195aa43
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:17:47 +0200

    Matrices

--- Intersection.java
+++ Intersection.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
-public class intersection {
+public class Intersection {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
