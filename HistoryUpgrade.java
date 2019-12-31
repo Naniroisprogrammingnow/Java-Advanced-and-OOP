3ab07a3325c4764fcf98d5a86c010d7e46341099
Adriana Panayotova <48690076+Naniroisprogrammingnow@users.noreply.github.com>
2019-12-31 10:16:26 +0200

    StacksAndQueues

--- HistoryUpgrade.java
+++ HistoryUpgrade.java
@@ -1,7 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class historyUpgrade {
+public class HistoryUpgrade {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayDeque<String> history = new ArrayDeque<>();
